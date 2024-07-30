754d179e3ce7d9c5cd0f48e42e43b7ff8183ca34
Nainah23 <kamauwainaina29@gmail.com>
2024-07-30 19:20:57 +0000

    Add all routes

--- 0x02-i18n/routes/routes_6.py
+++ 0x02-i18n/routes/routes_6.py
@@ -9,4 +9,4 @@ app_routes = Blueprint('app_routes', __name__)
 @app_routes.route('/', methods=["GET"], strict_slashes=False)
 def home():
     """ Home page """
-    return render_template('0-index.html')
+    return render_template('6-index.html')
